5f0e4a7046eba072760c68bc726cbe58fd7db3e8
Viet Tu <tupham1120@gmail.com>
2023-03-13 13:29:50 +0700

    Add file db.sql, LoaiSanPham.cs

---
+++ HeThongQuanLyNhaThuocLongChau/BusinessLogicLayer/LoaiSanPham.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeThongQuanLyNhaThuocLongChau.BusinessLogicLayer
+{
+    public class LoaiSanPham
+    {
+        public string maLoaiSP { get; set; }
+        public string tenLoaiSP { get; set; }
+
+        public LoaiSanPham()
+        {
+        }
+
+        public LoaiSanPham(string maLoaiSP, string tenLoaiSP)
+        {
+            this.maLoaiSP = maLoaiSP;
+            this.tenLoaiSP = tenLoaiSP;
+        }
+    }
+}
---
+++ HeThongQuanLyNhaThuocLongChau/DataAccessLayer/SanPhamDAL.cs
@@ -0,0 +1,16 @@
+﻿using HeThongQuanLyNhaThuocLongChau.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeThongQuanLyNhaThuocLongChau.DataAccessLayer
+{
+    class SanPhamDAL
+    {
+        ConnectDB connectDB = new ConnectDB(); 
+
+
+    }
+}
